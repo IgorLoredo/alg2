12f06734ae82adff30d30e2f00cdbfffd9725016
Igor Guilherme <igorloredo12@gmail.com>
2020-07-05 19:27:21 -0300

    melhorias

--- alg2/grafosBUSCA/busca/seach.c
+++ alg2/grafosBUSCA/busca/seach.c
@@ -6,9 +6,22 @@
 #include "matrizAd.h"
 
 
-int DFSIterativa(Grafo *grafo; ){
+tvertice DFSIterativa(Grafo *new, tvertice node, tpeso soma){
+    if(!new) return ERROGRAFO;
+    if(!node) return ERROVERTICE;
+    
+    if(new->Matriz[0][0] == tvertice ){
+        return node;
+    }
+    int i;
+    tvertice aux;
+    tvertice matriz[new->numVertices][new->numVertices];
+    for(i = 0; i < new->numVertices; i++)
+        matriz[i][i]= BRANCO;
 
+    for(){
 
-    return SUCESS;
+    }
+    return aux;
 }
 
--- alg2/grafosBUSCA/busca/seach.h
+++ alg2/grafosBUSCA/busca/seach.h
@@ -1,12 +1,20 @@
 #ifndef  seach
 #define seach
 
+#include "matrizAd.h"
+
 #define SUCESS 1
 #define ERRO -1
 #define ERROGRAFO -2
 #define ERROVERTICE -3
 
 
+
+#define BRANCO 0
+#define CINZA 1
+#define PRETO 2
+
+tvertice DFSIterativa(Grafo *, tvertice,tpeso );
 
 
 
